144bf60d3831c78fa8b443aea1deeea2bedcfece
Austn Hughes <austen.hughes.91@gmail.com>
2021-01-15 20:13:25 -0700

    added psuedocode and cleaded up vaibles

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCaseLetters = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-var uperCaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var specailCharacter = [" ","!","@","#","$","%","^","&","*","(",")","_","-","+","=","~","`","{","}","[","]",":",";","'","<",">","?",",",".","/","\","|"]
-
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("")
+var special = "!@#$%^&".split("")
+var numbers = "123456789".split("")
 
 // Write password to the #password input
 function writePassword() {
@@ -17,4 +16,31 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword,);
+
+// ```
+// GIVEN I need a new, secure password
+
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+
+// WHEN prompted for character types to include in the password
+// THEN I choose lowercase, uppercase, numeric, and/or special characters
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+
+// Start over 
+// // ```
